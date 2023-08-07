34e4a0c87e83c172bafbfd1b2d14afdb507dea17
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2023-08-07 19:32:32 +0300

    try start working

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,19 @@
-import { BaseHttp } from './services/http/base/base'
+import { iHttpParamSettings } from 'bx-rest/src/typification/rest/settings'
+import { Observable, of } from 'rxjs'
 
-export { BaseHttp }
+export declare class BaseHttp {
+    httpGet<T>(
+        url: string,
+        params: any,
+        textError: string,
+        settings: iHttpParamSettings
+    ): Observable<T | undefined>
+
+    httpPost<T>(url: string,
+                         params: any ,
+                         textError: string,
+                         settings: iHttpParamSettings
+    ): Observable<T | undefined>
+
+    handleError<T>(result?: T, textError?: string): Observable<T>
+}
