f8bd3565df77f96d6efe0f4d96d6464b5cb5eab1
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2023-08-14 17:35:56 +0300

    - add global class map - add export map result function

--- projects/bx-rest/src/functions/mapResult.ts
+++ projects/bx-rest/src/functions/mapResult.ts
@@ -1,6 +1,6 @@
-import iHttpAnswerBX from '../typification/rest/base/httpAnswerBX';
+import iHttpAnswerBX from '../typification/rest/base/httpAnswerBX'
 
-export function mapResult<T>(v: iHttpAnswerBX<T> | undefined): T | undefined {
+export function mapResult<T>(v: iHttpAnswerBX<T> | undefined) {
   if (v && (v.result || typeof v.result === 'boolean')) {
     return v.result
   }
--- projects/bx-rest/src/rest.ts
+++ projects/bx-rest/src/rest.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { BXRestUser } from './request/user'
 import BXRestLists from './request/lists'
+import BXRestListsMap from './map/lists'
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,13 @@ export class BXRest {
   constructor(
     public user: BXRestUser,
     public lists: BXRestLists
+  ) {
+  }
+}
+
+export class BXRestMap {
+  constructor(
+    public lists: BXRestListsMap
   ) {
   }
 }
