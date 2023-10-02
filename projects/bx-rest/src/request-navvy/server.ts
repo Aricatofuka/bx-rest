0e6d537e066eba6eb2a7bdae2499913d3710a577
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2023-10-02 11:41:30 +0300

    - Fix method server.time

--- projects/bx-rest/src/request-navvy/server.ts
+++ projects/bx-rest/src/request-navvy/server.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core'
 import { BXRestServer } from '../request/server'
 import { Navvy } from '../services/navvy'
+import { BXRestMapServer } from '../map/server'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BXRestNavvyServer {
 
-  protected Navvy: Navvy<BXRestServer, undefined>
+  protected Navvy: Navvy<BXRestServer, BXRestMapServer>
 
   constructor(
     private BXRestServer: BXRestServer,
+    public BXRestMapServer: BXRestMapServer
   ) {
-    this.Navvy = new Navvy(this.BXRestServer, undefined)
+    this.Navvy = new Navvy(this.BXRestServer, BXRestMapServer)
   }
 
   time(){
-    return this.Navvy.simple(this.BXRestServer.time, 'Не удалось получить время сервера')
+    return this.Navvy.simple(
+      this.BXRestServer.time,
+      'Не удалось получить время сервера',
+      this.BXRestMapServer.time
+    )
   }
 }
