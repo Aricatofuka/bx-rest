0bdb65670b9bd41f4d9b81c66de8f33adf5711c3
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2023-10-30 20:38:22 +0300

    - code refactoring - fix some bags

--- projects/bx-rest/src/map/tasks/task.ts
+++ projects/bx-rest/src/map/tasks/task.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core'
 import {
   iBXRestTasksTaskList,
   iBXRestTasksTaskListHttp,
-  } from '../../typification/rest/tasks/task/list'
+} from '../../typification/rest/tasks/task/list'
 import {
   iBXRestTasksTaskGetHttp, iBXRestTasksTaskGetHttpDefault
 } from '../../typification/rest/tasks/task/get'
@@ -17,12 +17,13 @@ import { iBXRestTaskFieldsName } from '../../typification/rest/tasks/base/fields
 @Injectable({
   providedIn: 'root'
 })
-export class BXRestMapTasksTask extends BaseMapServices{
+export class BXRestMapTasksTask extends BaseMapServices {
 
   constructor(public result: BXRestMapTasksTaskResult) {
     super()
   }
-  getaccess(v: iBXRestTaskGetAccess | undefined){
+
+  getaccess(v: iBXRestTaskGetAccess | undefined) {
     return (v && v.allowedActions) ? v.allowedActions : undefined
   }
 
@@ -30,51 +31,114 @@ export class BXRestMapTasksTask extends BaseMapServices{
     return (item) ? this.TaskBXHttpToTaskBX(item.task) : undefined
   }
 
-  get(item: iBXRestTasksTaskGetHttp<iBXRestTaskFieldsName[]> | undefined ) {
+  get(item: iBXRestTasksTaskGetHttp<iBXRestTaskFieldsName[]> | undefined) {
     return (item && item.task) ? this.TaskBXHttpToTaskBX(item.task) : undefined
   }
 
   list<S extends iBXRestTaskFieldsName[]>(item: iBXRestTasksTaskListHttp<S> | undefined) {
     // @ts-ignore
-    return (item && item.tasks) ?  item.tasks.map( i =>  this.TaskBXHttpToTaskBX<iBXRestTasksTaskListHttp<S>, iBXRestTasksTaskList<S>>(i)) : undefined
+    return (item && item.tasks) ? item.tasks.map(i => this.TaskBXHttpToTaskBX<iBXRestTasksTaskListHttp<S>, iBXRestTasksTaskList<S>>(i)) : undefined
   }
 
   private TaskBXHttpToTaskBX<T extends Partial<iBXRestHttpTask>, R>(item: T): R {
     let result: any = {}
-    if(item.id) { result.id = this.toNum(item.id) }
-    if(item.parentId) { result.parentId = this.toNum(item.parentId) }
-    if(item.priority) { result.priority = this.toNum(item.priority) }
-    if(item.status) { result.status = this.toNum(item.status) }
-    if(item.multitask) { result.multitask = item.multitask === 'Y' }
-    if(item.groupId) { result.groupId = this.toNum(item.groupId) }
-    if(item.stageId) { result.stageId = this.toNum(item.stageId) }
-    if(item.createdBy) { result.createdBy = this.toNum(item.createdBy) }
-    if(item.createdDate) { result.createdDate = this.toDate(item.createdDate) }
-    if(item.responsibleId) { result.responsibleId = this.toNum(item.responsibleId) }
-    if(item.changedBy) { result.changedBy = this.toNum(item.changedBy) }
-    if(item.changedDate) { result.changedDate = this.toDate(item.changedDate) }
-    if(item.statusChangedBy) { result.statusChangedBy = this.toNum(item.statusChangedBy) }
-    if(item.statusChangedDate) { result.statusChangedDate = this.toDate(item.statusChangedDate) }
-    if(item.closedBy) { result.closedBy = this.toNum(item.closedBy) }
-    if(item.closedDate) { result.closedDate = this.toDate(item.closedDate) }
-    if(item.dateStart) { result.dateStart = this.toDate(item.dateStart) }
-    if(item.deadline) { result.deadline = this.toDate(item.deadline) }
-    if(item.startDatePlan) { result.startDatePlan = this.toDate(item.startDatePlan) }
-    if(item.endDatePlan) { result.endDatePlan = this.toDate(item.endDatePlan) }
-    if(item.forumTopicId) { result.forumTopicId = this.toNum(item.forumTopicId) }
-    if(item.forumId) { result.forumId = this.toNum(item.forumId) }
-    if(item.viewedDate) { result.viewedDate = this.toDate(item.viewedDate) }
-    if(item.sorting) { result.sorting = this.toNum(item.sorting) }
-    if(item.durationPlan) { result.durationPlan = this.toNum(item.durationPlan) }
-    if(item.descriptionInBbcode) { result.descriptionInBbcode = item.descriptionInBbcode === 'Y' }
-    if(item.accomplices) { result.accomplices = item.accomplices.map(i =>this.toNum(i)) }
-    if(item.subStatus) { result.subStatus = this.toNum(item.subStatus) }
-    if(item.creator) { result.creator = Object.assign(item.creator, {id: this.toNum(item.creator.id)}) }
-    if(item.responsible) { result.responsible = Object.assign(item.responsible, {id: this.toNum(item.responsible.id)}) }
-    if(item.accomplicesData) {
+    if (item.id) {
+      result.id = this.toNum(item.id)
+    }
+    if (item.parentId) {
+      result.parentId = this.toNum(item.parentId)
+    }
+    if (item.priority) {
+      result.priority = this.toNum(item.priority)
+    }
+    if (item.status) {
+      result.status = this.toNum(item.status)
+    }
+    if (item.multitask) {
+      result.multitask = item.multitask === 'Y'
+    }
+    if (item.groupId) {
+      result.groupId = this.toNum(item.groupId)
+    }
+    if (item.stageId) {
+      result.stageId = this.toNum(item.stageId)
+    }
+    if (item.createdBy) {
+      result.createdBy = this.toNum(item.createdBy)
+    }
+    if (item.createdDate) {
+      result.createdDate = this.toDate(item.createdDate)
+    }
+    if (item.responsibleId) {
+      result.responsibleId = this.toNum(item.responsibleId)
+    }
+    if (item.changedBy) {
+      result.changedBy = this.toNum(item.changedBy)
+    }
+    if (item.changedDate) {
+      result.changedDate = this.toDate(item.changedDate)
+    }
+    if (item.statusChangedBy) {
+      result.statusChangedBy = this.toNum(item.statusChangedBy)
+    }
+    if (item.statusChangedDate) {
+      result.statusChangedDate = this.toDate(item.statusChangedDate)
+    }
+    if (item.closedBy) {
+      result.closedBy = this.toNum(item.closedBy)
+    }
+    if (item.closedDate) {
+      result.closedDate = this.toDate(item.closedDate)
+    }
+    if (item.dateStart) {
+      result.dateStart = this.toDate(item.dateStart)
+    }
+    if (item.deadline) {
+      result.deadline = this.toDate(item.deadline)
+    }
+    if (item.startDatePlan) {
+      result.startDatePlan = this.toDate(item.startDatePlan)
+    }
+    if (item.endDatePlan) {
+      result.endDatePlan = this.toDate(item.endDatePlan)
+    }
+    if (item.forumTopicId) {
+      result.forumTopicId = this.toNum(item.forumTopicId)
+    }
+    if (item.forumId) {
+      result.forumId = this.toNum(item.forumId)
+    }
+    if (item.viewedDate) {
+      result.viewedDate = this.toDate(item.viewedDate)
+    }
+    if (item.sorting) {
+      result.sorting = this.toNum(item.sorting)
+    }
+    if (item.durationPlan) {
+      result.durationPlan = this.toNum(item.durationPlan)
+    }
+    if (item.descriptionInBbcode) {
+      result.descriptionInBbcode = item.descriptionInBbcode === 'Y'
+    }
+    if (item.accomplices) {
+      result.accomplices = item.accomplices.map(i => this.toNum(i))
+    }
+    if (item.subStatus) {
+      result.subStatus = this.toNum(item.subStatus)
+    }
+    if (item.creator) {
+      result.creator = Object.assign(item.creator, {id: this.toNum(item.creator.id)})
+    }
+    if (item.responsible) {
+      result.responsible = Object.assign(item.responsible, {id: this.toNum(item.responsible.id)})
+    }
+    if (item.accomplicesData) {
       result.accomplicesData =
-        Object.values(item.accomplicesData).map(i => Object.assign(i, {id: this.toNum(i.id)}))  }
-    if(item.allowTimeTracking) { result.allowTimeTracking = item.allowTimeTracking === 'Y' }
+        Object.values(item.accomplicesData).map(i => Object.assign(i, {id: this.toNum(i.id)}))
+    }
+    if (item.allowTimeTracking) {
+      result.allowTimeTracking = item.allowTimeTracking === 'Y'
+    }
 
     return Object.assign(item, result)
   }
