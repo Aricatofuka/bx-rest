6968556df6dd39f88a54bf96ceee8883bcbeda5e
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2023-11-03 20:30:48 +0300

    - added the ability to make custom requests through the BXRestRequest service

--- projects/bx-rest/src/public-api.ts
+++ projects/bx-rest/src/public-api.ts
@@ -1,9 +1,10 @@
 /*
  * Public API Surface of bx-rest
  */
-export { BX_REST_SETTINGS } from './settings';
+export { BX_REST_SETTINGS } from './settings'
 export { BXRestMapResult } from './functions/mapResult'
 export { BXRest, BXRestNavvy, BXRestMap } from './rest'
 export { Navvy } from './services/navvy'
 export { Map as BXMap } from './map'
 export * as partNameMethods from './consts/part-name-methods'
+export { HttpBXServices as BXRestRequest } from './services/http/HttpBX'
