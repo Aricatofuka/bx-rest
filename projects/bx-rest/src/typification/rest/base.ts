fbf7891e89450b8b4afbac108cc96924c4c7384b
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2025-12-29 19:17:28 +0300

    - add base interface paths

--- projects/bx-rest/src/typification/rest/base.ts
+++ projects/bx-rest/src/typification/rest/base.ts
@@ -3,3 +3,8 @@
  */
 export * from './base/answer'
 export * from './base/api-pagination-bx'
+export * from './base/yes-no'
+export * from './base/sort'
+export * from './base/order-options'
+export * from './base/filter-generator'
+export * from './base/field-bx-type'
