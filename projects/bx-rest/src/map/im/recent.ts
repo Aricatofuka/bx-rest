1d121b7db65c6cfd6da11391395f3c2bd0b4a96e
Aricatofuka <52385430+Aricatofuka@users.noreply.github.com>
2026-04-14 11:53:07 +0300

    Фикс - dependencies

--- projects/bx-rest/src/map/im/recent.ts
+++ projects/bx-rest/src/map/im/recent.ts
@@ -20,26 +20,8 @@ export class BXRestMapImRecent {
           ...{
             date_update: toDate(i.date_update),
             counter: toNum(i.counter),
-            user: i.user && i.user.id
-              ? {
-                ...i.user,
-                ...{
-                  id: toNum(i.user.id),
-                  last_activity_date: i.user.last_activity_date ? toDate(i.user.last_activity_date) : false,
-                  mobile_last_date: i.user.mobile_last_date ? toDate(i.user.mobile_last_date) : false,
-                  absent: i.user.absent ? toDate(i.user.absent) : false,
-                }
-              }
-              : undefined,
-            chat: i.chat && BXRestMapImRecent.isIBXRestImDialogRecentGetChatChatDataHttp(i.chat)
-              ? {
-                ...i.chat,
-                ...{
-                  id: toNum(i.chat.id),
-                  date_create: toDate(i.chat.date_create)
-                }
-              }
-              : i.chat,
+            user: BXRestMapImRecent.mapUser(i.user),
+            chat: BXRestMapImRecent.mapChat(i.chat),
           }
         }))
         : undefined
@@ -77,5 +59,35 @@ export class BXRestMapImRecent {
 
   static isIBXRestImDialogRecentGetChatChatDataHttp(item: iBXRestImDialogRecentGetChatChatDataHttp | iBXRestImDialogRecentGetChatUserDataBase): item is iBXRestImDialogRecentGetChatChatDataHttp {
     return 'id' in item
+  }
+
+  private static mapUser(user: iBXRestImDialogRecentGetHttp['user']): iBXRestImDialogRecentGet['user'] {
+    if (!user || !user.id) {
+      return undefined
+    }
+
+    return {
+      ...user,
+      id: toNum(user.id),
+      last_activity_date: user.last_activity_date ? toDate(user.last_activity_date) : false,
+      mobile_last_date: user.mobile_last_date ? toDate(user.mobile_last_date) : false,
+      absent: user.absent ? toDate(user.absent) : false,
+    }
+  }
+
+  private static mapChat(chat: iBXRestImDialogRecentGetHttp['chat']): iBXRestImDialogRecentGet['chat'] {
+    if (!chat) {
+      return undefined
+    }
+
+    if (BXRestMapImRecent.isIBXRestImDialogRecentGetChatChatDataHttp(chat)) {
+      return {
+        ...chat,
+        id: toNum(chat.id),
+        date_create: toDate(chat.date_create)
+      }
+    }
+
+    return chat
   }
 }
